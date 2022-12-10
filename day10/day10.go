c9ed81f802dc799c70e82c0f60342faf00164ca3
Thorsten Lanfer <tlanfer@gmail.com>
2022-12-10 14:57:50 +0100

    day 10

---
+++ day10/day10.go
@@ -0,0 +1,77 @@
+package day10
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func NewComputer() *Computa {
+	return &Computa{
+		x: 1,
+	}
+}
+
+type Computa struct {
+	cycles int
+	x      int
+
+	signalSum int
+}
+
+func (d *Computa) Run(input string) int {
+	instructions := strings.Split(input, "\n")
+
+	for _, instruction := range instructions {
+		ps := strings.Split(instruction, " ")
+
+		cmd := ps[0]
+
+		switch cmd {
+		case "noop":
+			d.noop()
+		case "addx":
+			d.addx(ps[1])
+		}
+	}
+
+	fmt.Println("")
+
+	return d.signalSum
+}
+
+func (d *Computa) clock() {
+	d.pixel()
+	d.cycles++
+	if (d.cycles-20)%40 == 0 {
+		signalStrength := d.cycles * d.x
+		d.signalSum += signalStrength
+		//log.Printf("Signal strength at %v * %v = %v (Total %v)", d.cycles, d.x, signalStrength, d.signalSum)
+	}
+}
+
+func (d *Computa) noop() {
+	d.clock()
+}
+
+func (d *Computa) addx(s string) {
+	d.clock()
+	d.clock()
+	x, _ := strconv.Atoi(s)
+	d.x += x
+}
+
+func (d *Computa) pixel() {
+
+	pos := d.cycles % 40
+
+	if pos >= d.x-1 && pos <= d.x+1 {
+		fmt.Print("#")
+	} else {
+		fmt.Print(".")
+	}
+
+	if pos == 39 {
+		fmt.Print("\n")
+	}
+}
---
+++ day10/day10_test.go
@@ -0,0 +1,320 @@
+package day10_test
+
+import (
+	"adventofcode2022/day10"
+	"testing"
+)
+
+func TestDay10(t *testing.T) {
+
+	inputs := map[string]struct {
+		input    string
+		expected int
+	}{
+		"test":   {input: testInput, expected: 13140},
+		"actual": {input: actualInput, expected: 15220},
+	}
+
+	for name, tc := range inputs {
+		t.Run(name, func(t *testing.T) {
+			d := day10.NewComputer()
+			actual := d.Run(tc.input)
+
+			if actual != tc.expected {
+				t.Fatalf("signal strength expected = %v, was %v", tc.expected, actual)
+			}
+		})
+	}
+
+}
+
+const testInput = `addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop`
+
+const actualInput = `noop
+noop
+noop
+addx 5
+addx 1
+addx 4
+addx 1
+noop
+addx 4
+noop
+addx 1
+addx 4
+addx 8
+addx -7
+addx 3
+addx 1
+noop
+addx 4
+addx 2
+addx 5
+addx -1
+noop
+addx -37
+noop
+noop
+addx 3
+addx 2
+addx 13
+addx 12
+addx -15
+addx -2
+addx 2
+addx -11
+addx 18
+addx 2
+addx -15
+addx 16
+addx 5
+addx 2
+addx 5
+noop
+noop
+noop
+addx 3
+addx -2
+addx -38
+noop
+addx 3
+addx 4
+noop
+noop
+noop
+noop
+noop
+addx 5
+addx 5
+noop
+noop
+addx 21
+addx -17
+addx 6
+noop
+noop
+noop
+noop
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx 3
+addx 5
+addx -38
+noop
+noop
+addx 5
+addx -2
+addx 1
+addx 7
+noop
+addx 22
+addx -18
+addx -11
+addx 27
+addx -13
+addx 2
+addx 5
+addx -8
+addx 9
+addx 2
+noop
+addx 7
+noop
+addx 1
+noop
+addx -38
+noop
+addx 2
+addx 5
+addx -3
+noop
+addx 8
+addx 11
+addx -6
+noop
+addx 24
+addx -31
+addx 10
+addx 2
+addx 5
+addx 3
+noop
+addx 2
+addx -29
+addx 21
+addx 11
+addx 5
+addx -39
+addx 4
+addx -2
+addx 2
+addx 7
+noop
+addx -1
+addx 2
+noop
+addx 4
+noop
+addx 1
+addx 2
+addx 5
+addx 2
+noop
+noop
+addx -6
+addx 9
+addx -18
+addx 25
+addx 3
+noop
+addx -17
+noop`
