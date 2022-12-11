d05146a7acb9b66f81f12ee0fba482e919f6b43a
Thorsten Lanfer <tlanfer@gmail.com>
2022-12-11 19:11:09 +0100

    add fireeyes input

--- day10/day10_test.go
+++ day10/day10_test.go
@@ -11,8 +11,9 @@ func TestDay10(t *testing.T) {
 		input    string
 		expected int
 	}{
-		"test":   {input: testInput, expected: 13140},
-		"actual": {input: actualInput, expected: 15220},
+		"test":    {input: testInput, expected: 13140},
+		"actual":  {input: actualInput, expected: 15220},
+		"fireeye": {input: fireeyeinput, expected: 12740},
 	}
 
 	for name, tc := range inputs {
@@ -317,4 +318,142 @@ addx 25
 addx 3
 noop
 addx -17
+noop`
+
+const fireeyeinput = `noop
+noop
+noop
+addx 4
+addx 1
+addx 5
+addx 1
+addx 5
+noop
+addx -1
+addx -6
+addx 11
+noop
+noop
+noop
+noop
+addx 6
+addx 5
+noop
+noop
+noop
+addx -30
+addx 34
+addx 2
+addx -39
+noop
+addx 5
+addx 2
+addx 19
+addx -18
+addx 2
+addx 5
+addx 2
+addx 3
+noop
+addx 2
+addx 3
+noop
+addx 2
+addx 3
+noop
+addx 2
+addx 3
+noop
+addx 2
+addx -15
+addx -22
+noop
+noop
+addx 5
+addx 2
+noop
+noop
+addx 14
+addx -11
+addx 5
+addx 2
+addx 3
+noop
+addx 2
+addx -16
+addx 17
+addx 2
+addx 5
+addx 2
+addx -6
+addx -25
+addx 35
+addx 1
+addx -36
+addx 1
+addx 22
+addx -19
+addx 5
+addx 2
+noop
+noop
+addx 5
+noop
+noop
+noop
+addx 1
+addx 4
+noop
+noop
+noop
+addx 5
+noop
+addx 1
+addx 2
+addx 3
+addx 4
+addx -34
+addx 21
+addx -24
+addx 2
+addx 5
+addx 7
+addx -6
+addx 2
+addx 30
+addx -23
+addx 10
+addx -9
+addx 2
+addx 2
+addx 5
+addx -12
+addx 13
+addx 2
+addx 5
+addx 2
+addx -12
+addx -24
+addx -1
+noop
+addx 3
+addx 3
+addx 1
+addx 5
+addx 21
+addx -16
+noop
+addx 19
+addx -18
+addx 2
+addx 5
+addx 2
+addx 3
+noop
+addx 3
+addx -1
+addx 1
+addx 2
+addx -18
+addx 1
 noop`
